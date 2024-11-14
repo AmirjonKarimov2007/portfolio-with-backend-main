55c6282fcc1d5a83db6bd4833e2be2e551e6350b
Amirjon Karimov <amirjonkarimoff@gmail.com>
2024-11-14 22:11:55 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amirjon_website',
+        'USER': 'amirjon',
+        'PASSWORD': 'Karimoff2007',
+        'HOST': 'postgresql-amirjon.alwaysdata.net',
+        'PORT': 5432,
     }
 }
 
